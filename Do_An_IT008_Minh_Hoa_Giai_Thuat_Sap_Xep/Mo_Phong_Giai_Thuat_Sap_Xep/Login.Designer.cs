338cacd68815bee305938ccdb48886cbc9c69494
VoPhuPhong76 <phongvo220406@gmail.com>
2025-12-21 23:16:49 +0700

    Update comments/Signup.cs
    21/12/2025-11:16

---
+++ Do_An_IT008_Minh_Hoa_Giai_Thuat_Sap_Xep/Mo_Phong_Giai_Thuat_Sap_Xep/Login.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace Mo_Phong_Giai_Thuat_Sap_Xep
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.UsernameLbl = new System.Windows.Forms.Label();
+            this.PsswrdLbl = new System.Windows.Forms.Label();
+            this.LoginBtn = new System.Windows.Forms.Button();
+            this.pictureBoxEye = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SignUpLink = new System.Windows.Forms.LinkLabel();
+            this.ShowpassCheck = new System.Windows.Forms.CheckBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxEye)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Location = new System.Drawing.Point(181, 87);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(150, 22);
+            this.txtUsername.TabIndex = 0;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(181, 123);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(150, 22);
+            this.txtPassword.TabIndex = 1;
+            this.txtPassword.UseSystemPasswordChar = true;
+            // 
+            // UsernameLbl
+            // 
+            this.UsernameLbl.AutoSize = true;
+            this.UsernameLbl.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.UsernameLbl.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.UsernameLbl.Location = new System.Drawing.Point(12, 83);
+            this.UsernameLbl.Name = "UsernameLbl";
+            this.UsernameLbl.Size = new System.Drawing.Size(154, 25);
+            this.UsernameLbl.TabIndex = 3;
+            this.UsernameLbl.Text = "Tên đăng nhập :";
+            // 
+            // PsswrdLbl
+            // 
+            this.PsswrdLbl.AutoSize = true;
+            this.PsswrdLbl.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PsswrdLbl.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.PsswrdLbl.Location = new System.Drawing.Point(12, 119);
+            this.PsswrdLbl.Name = "PsswrdLbl";
+            this.PsswrdLbl.Size = new System.Drawing.Size(107, 25);
+            this.PsswrdLbl.TabIndex = 4;
+            this.PsswrdLbl.Text = "Mật Khẩu :";
+            // 
+            // LoginBtn
+            // 
+            this.LoginBtn.BackColor = System.Drawing.Color.SteelBlue;
+            this.LoginBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.LoginBtn.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LoginBtn.ForeColor = System.Drawing.Color.White;
+            this.LoginBtn.Location = new System.Drawing.Point(146, 169);
+            this.LoginBtn.Name = "LoginBtn";
+            this.LoginBtn.Size = new System.Drawing.Size(115, 41);
+            this.LoginBtn.TabIndex = 3;
+            this.LoginBtn.Text = "Login";
+            this.LoginBtn.UseVisualStyleBackColor = false;
+            this.LoginBtn.Click += new System.EventHandler(this.LoginBtn_Click);
+            // 
+            // pictureBoxEye
+            // 
+            this.pictureBoxEye.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBoxEye.Image = global::Mo_Phong_Giai_Thuat_Sap_Xep.Properties.Resources.close_eye;
+            this.pictureBoxEye.InitialImage = null;
+            this.pictureBoxEye.Location = new System.Drawing.Point(338, 127);
+            this.pictureBoxEye.Name = "pictureBoxEye";
+            this.pictureBoxEye.Size = new System.Drawing.Size(20, 18);
+            this.pictureBoxEye.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBoxEye.TabIndex = 12;
+            this.pictureBoxEye.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.Control;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.IndianRed;
+            this.label1.Location = new System.Drawing.Point(134, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(143, 29);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Đăng Nhập";
+            // 
+            // SignUpLink
+            // 
+            this.SignUpLink.AutoSize = true;
+            this.SignUpLink.Location = new System.Drawing.Point(105, 242);
+            this.SignUpLink.Name = "SignUpLink";
+            this.SignUpLink.Size = new System.Drawing.Size(172, 16);
+            this.SignUpLink.TabIndex = 4;
+            this.SignUpLink.TabStop = true;
+            this.SignUpLink.Text = "Chưa có tài khoản? Đăng ký";
+            this.SignUpLink.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.SignUpLink_LinkClicked);
+            // 
+            // ShowpassCheck
+            // 
+            this.ShowpassCheck.AutoSize = true;
+            this.ShowpassCheck.Location = new System.Drawing.Point(364, 128);
+            this.ShowpassCheck.Name = "ShowpassCheck";
+            this.ShowpassCheck.Size = new System.Drawing.Size(18, 17);
+            this.ShowpassCheck.TabIndex = 2;
+            this.ShowpassCheck.UseVisualStyleBackColor = true;
+            this.ShowpassCheck.CheckedChanged += new System.EventHandler(this.ShowpassCheck_CheckedChanged);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Mo_Phong_Giai_Thuat_Sap_Xep.Properties.Resources.channels4_profile_Photoroom;
+            this.pictureBox1.Location = new System.Drawing.Point(317, 19);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(40, 39);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 13;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::Mo_Phong_Giai_Thuat_Sap_Xep.Properties.Resources.dhuit_Photoroom;
+            this.pictureBox2.Location = new System.Drawing.Point(22, 6);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(98, 72);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 14;
+            this.pictureBox2.TabStop = false;
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(400, 282);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.LoginBtn);
+            this.Controls.Add(this.SignUpLink);
+            this.Controls.Add(this.pictureBoxEye);
+            this.Controls.Add(this.ShowpassCheck);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtUsername);
+            this.Controls.Add(this.UsernameLbl);
+            this.Controls.Add(this.PsswrdLbl);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.KeyPreview = true;
+            this.MaximizeBox = false;
+            this.Name = "Login";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Đăng Nhập";
+            this.Load += new System.EventHandler(this.Login_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Login_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxEye)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label UsernameLbl;
+        private System.Windows.Forms.Label PsswrdLbl;
+        private System.Windows.Forms.Button LoginBtn;
+        private System.Windows.Forms.LinkLabel SignUpLink;
+        private System.Windows.Forms.CheckBox ShowpassCheck;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBoxEye;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
